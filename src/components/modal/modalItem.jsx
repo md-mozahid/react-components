4229e95dbeb581010182cb64631ed8ceafebd87f
mozahid <ce.muzahid@gmail.com>
2023-11-04 07:16:11 +0600

    add modal component

--- src/components/modal/modalItem.jsx
+++ src/components/modal/modalItem.jsx
@@ -1,15 +1,24 @@
+import { motion } from 'framer-motion'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 import { images } from '../../constant'
 import './modalItem.css'
 
-const ModalItem = ({modalOpen, setModalOpen}) => {
+const ModalItem = ({ setModalOpen }) => {
   return (
     <>
-      <div className="card-item">
-        <img className="" src={images.about03} alt="image" />
-        <div className="card-title">
-          <h3 className="">Project Title.</h3>
-        </div>
-      </div>
+      <motion.div
+        animate={{ opacity: [0, 1] }}
+        transition={{
+          ease: 'linear',
+          duration: 1,
+        }}
+        className="modal-content centered"
+        onClick={() => setModalOpen(false)}>
+        <button className="close" onClick={() => setModalOpen(false)}>
+          <AiOutlineCloseCircle />
+        </button>
+        <img src={images.asus} alt="" />
+      </motion.div>
     </>
   )
 }
